9bfd02af62863c7ce9c47d955bae787c997975e1
Ian Nguyen <ianxmnguyen@gmail.com>
2024-06-11 05:14:07 -0700

    Add files via upload
    Was running into trouble with code and tried to fix it last minute.
    Couldn't fix it, but I prefer this code better than the last one.
    If editting it after the due date is considered a zero, I apologize. I'm willing to take the 0 on this assignment if so. Please let me know in the comments when grading! Thank you

--- VBA-Challenge-Code.vb
+++ VBA-Challenge-Code.vb
@@ -1,152 +1,103 @@
-Sub moduleTwoChallenge():
-    
-    'Loop for each and every worksheet
+Sub vbaChallenge()
+    'Dim worksheet for loop
     Dim ws As Worksheet
     For Each ws In Worksheets
-    
-    'Set all Dimensions and add values to variables
-    'Assign variable to lastrow
-        Dim tickerName As String
-        Dim percentChange As Double
-        Dim LastRowA As Double
-            LastRowA = ws.Cells(Rows.Count, 1).End(xlUp).Row
-        Dim LastRowB As Double
-            LastRowB = ws.Cells(Rows.Count, 9).End(xlUp).Row
-        Dim gIncrease As Double
-        Dim gDecrease As Double
-        Dim gVolume As Double
-        Dim i As Double
-        Dim k As Double
-        Dim n As Double
+        'Identify the given values and what we're trying to find
+        Dim tName As String
+        Dim qChange As Double
+        Dim pChange As Double
+        Dim tVol As Double
+        Dim LastRowA As Long
+        LastRowA = ws.Cells(ws.Rows.Count, 1).End(xlUp).Row
+        Dim LastRowB As Long
+        LastRowB = ws.Cells(ws.Rows.Count, 2).End(xlUp).Row
+        'Dim what the tools we will use to do our calculation
+        Dim i As Long
+        Dim k As Long
+        k = 2
         Dim oPrice As Double
+        oPrice = ws.Cells(2, 3).Value
         Dim cPrice As Double
-        
-        
-        'To refer to all worksheets in workbook
-        WorksheetName = ws.Name
-    
-        
-        'Start of Data Extraction and Analysis
+        'Dim for Summary Tabble
+        Dim gVolume As Double
+        Dim gInc As Double
+        Dim gDec As Double
+        'Create Header for Output Table
         ws.Cells(1, 9).Value = "Ticker"
         ws.Cells(1, 10).Value = "Quarterly Change"
         ws.Cells(1, 11).Value = "Percent Change"
         ws.Cells(1, 12).Value = "Total Stock Volume"
-        
-        k = 2
-        n = 2
-        
-            'Look up to see how to find and assign last row of a column
-            'Start of Overall Loop
-            For i = 2 To LastRowA
-            oPrice = ws.Cells(k, 3).Value
-            cPrice = ws.Cells(i, 6).Value
-            
-            'Apply Ticker Name to appropriate column
-            ws.Cells(i, 9).Value = ws.Cells(i, 1).Value
-            
-                'Start of Nested loop1
-                If ws.Cells(i, 1).Value <> ws.Cells(i + 1, 1).Value Then
-                'Quarterly Change = Close - Open
-                ws.Cells(i, 10).Value = cPrice - oPrice
-                
-                'Insert Nested loop for color assignment to Quarterly Change
-                    'Start of Nested loop2
-                    If ws.Cells(i, 10).Value < 0 Then
-                    'Red
-                    ws.Cells(2, 10).Interior.ColorIndex = 3
-                
-                    Else
-                    'Green
-                    ws.Cells(2, 10).Interior.ColorIndex = 4
-                
-                    End If
-                    'Make sure open doesn't = 0 because you cant divide by 0 (Will come back as an error!)
-                    If ws.Cells(k, 3).Value <> 0 Then
-                    'percentChange= ((earliest value - later value)/earliest value)*100
-                    percentChange = ((oPrice - cPrice) / oPrice)
-                    
-                    'Look up how to change formating into "Percent" to negate having to *100 in formula
-                    
-                    ws.Cells(2, 11).Value = Format(percentChange, "Percent")
-                    
-                    Else
-                    '0 means false in this case
-                    ws.Cells(2, 11).Value = Format(0, "Percent")
-                    'End of nested Loop2
-                    End If
-                    
-                'Total Volume =
-                ws.Cells(2, 12).Value = WorksheetFunction.Sum(Range(ws.Cells(k, 7), ws.Cells(i, 7)))
-                
-                qChange = n + 1
-                
-                k = i + 1
-                'End of nested loop1
+        'Start of nestedLoop1 for Output Table
+        'Ensure to use the LastRow Forumla
+        For i = 2 To LastRowA
+            'Make sure to combine all the Tickernames together
+            If ws.Cells(i + 1, 1).Value <> ws.Cells(i, 1).Value Then
+                tName = ws.Cells(i, 1).Value
+                tVol = tVol + ws.Cells(i, 7).Value
+                cPrice = ws.Cells(i, 6).Value
+                'Calc the quarter change = closePrice - openPrice
+                qChange = cPrice - oPrice
+                'Assign value to populate in cells
+                ws.Cells(k, 10).Value = qChange
+                ws.Cells(k, 9).Value = tName
+                'Start of nested Loop2
+                'Assign color index to show the quarterly changes that are <0 (Red) or >0 (Green)
+                If qChange < 0 Then
+                    ws.Cells(k, 10).Interior.ColorIndex = 3 'Red
+                Else
+                    ws.Cells(k, 10).Interior.ColorIndex = 4 'Green
                 End If
-            'End of overall loop
-            Next i
-        
-        'Start of Summary Table Code
-        'Assign headers for Summary Table
+                'Format cells into Percet
+                If oPrice <> 0 Then
+                    pChange = qChange / oPrice
+                    ws.Cells(k, 11).Value = Format(pChange, "Percent")
+                Else
+                '0 in this case means false
+                    ws.Cells(k, 11).Value = Format(0, "Percent")
+                End If
+                'End of nestedLoop2
+                tVol = 0
+                k = k + 1
+            Else
+                tVol = tVol + ws.Cells(i, 7).Value
+                ws.Cells(k, 12).Value = tVol
+            End If
+            'End of nestedLoop1 for Output Table
+        Next i
+
+        'Summary Table Code
+        'Assign Header cells
         ws.Cells(2, 15).Value = "Greatest % Increase"
         ws.Cells(3, 15).Value = "Greatest % Decrease"
         ws.Cells(4, 15).Value = "Greatest Total Volume"
         ws.Cells(1, 16).Value = "Ticker"
         ws.Cells(1, 17).Value = "Value"
-        
-        'Set Values to Cells
+        'Assign where to get values from
         gVolume = ws.Cells(2, 12).Value
-        gIncrease = ws.Cells(2, 11).Value
-        gDecrease = ws.Cells(2, 11).Value
-        Ticker = ws.Cells(1, 16).Value
-        Value = ws.Cells(1, 17).Value
-        gIncName = ws.Cells(2, 16).Value
-        gDecName = ws.Cells(3, 16).Value
-        gVolName = ws.Cells(4, 16).Value
-        
-            For i = 2 To LastRowB
-            
-            
-                If ws.Cells(i, 11).Value > gIncrease Then
-                gIncrease = ws.Cells(i, 11).Value
-                'Remember to have the tName to follow with the data
-                gIncName = ws.Cells(i, 9).Value
-                
-                Else
-                
-                gIncrease = gIncrease
-                
-                End If
-                
-                If ws.Cells(i, 11).Value < gDecrease Then
-                gDecrease = ws.Cells(i, 11).Value
-                'tName to follow the data
-                wDecName = ws.Cells(i, 9).Value
-                
-                Else
-                
-                gDecrease = gDecrease
-                
-                End If
-                
-                If ws.Cells(i, 12).Value > gVolume Then
+        gInc = ws.Cells(2, 11).Value
+        gDec = ws.Cells(2, 11).Value
+
+        For i = 2 To LastRowB
+            'Keep looping to check through the column and compare until they find the largest increase
+            If ws.Cells(i, 11).Value > gInc Then
+                gInc = ws.Cells(i, 11).Value
+                ws.Cells(2, 16).Value = ws.Cells(i, 9).Value
+                ws.Cells(2, 17).Value = Format(gInc, "Percent")
+            End If
+
+            'Keep looping to check through the column and compare until they find the largest decrease
+            If ws.Cells(i, 11).Value < gDec Then
+                gDec = ws.Cells(i, 11).Value
+                ws.Cells(3, 16).Value = ws.Cells(i, 9).Value
+                ws.Cells(3, 17).Value = Format(gDec, "Percent")
+            End If
+
+            'Keep looping to check through the column and compare until they find the largest volume
+            If ws.Cells(i, 12).Value > gVolume Then
                 gVolume = ws.Cells(i, 12).Value
-                't name to follow the data
-                gVolName = ws.Cells(i, 9).Value
-                
-                Else
-                
-                gVolume = gVolume
-                
-                End If
-                
-                
-            'Format Value
-            ws.Cells(4, 17).Value = Format(gVolume, "Scientific")
-            ws.Cells(2, 17).Value = Format(gIncrease, "Percent")
-            ws.Cells(3, 17).Value = Format(gDecrease, "Percent")
-            
-            Next i
+                ws.Cells(4, 16).Value = ws.Cells(i, 9).Value
+                ws.Cells(4, 17).Value = Format(gVolume, "Scientific")
+            End If
+        Next i
     Next ws
-        
 End Sub
